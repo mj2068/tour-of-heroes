69ae56338f6855ff8a511759b00790b8857c65bd
mj2068 <yuelianghushenfu@sina.com>
2021-11-01 02:35:34 +0800

    pt2 of tutorial done. select hero, display details

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 100, name: '24' },
+  { id: 101, name: 'X2' },
+  { id: 102, name: 'Max' },
+  { id: 103, name: 'Jiu' },
+  { id: 104, name: 'Jovia' },
+  { id: 105, name: 'The Wave' },
+  { id: 106, name: 'Berry' },
+  { id: 107, name: 'Pex' },
+  { id: 108, name: 'Lon' },
+  { id: 109, name: 'Signal' },
+  { id: 110, name: 'Dr. Lean' },
+  { id: 111, name: 'ChoCho' },
+];
