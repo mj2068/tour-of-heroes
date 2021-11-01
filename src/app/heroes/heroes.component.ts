b640c1447966260cb802ecf784cf0ea9f6bca12b
mj2068 <yuelianghushenfu@sina.com>
2021-11-01 20:34:03 +0800

    pt3 done. seperated heroes list and hero detail.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -10,7 +10,6 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes = HEROES;
-
   constructor() {}
 
   ngOnInit(): void {}
