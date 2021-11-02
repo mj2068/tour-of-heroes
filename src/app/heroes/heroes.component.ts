82c4f8d2ff77c83b42e86c88467b91e719a92b01
mj2068 <yuelianghushenfu@sina.com>
2021-11-02 16:17:00 +0800

    pt5 done. add routing.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,26 +8,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     console.log('@HeroesComponent.ngOnInit()');
-
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroComponent selected hero: ${hero.id}`);
-  }
-
   getHeroes() {
-    // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
